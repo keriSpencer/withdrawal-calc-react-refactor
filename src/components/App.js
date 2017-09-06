0a77d8021a9869053f37058f0ee9a4f2c30d201b
Keri Spencer <kerigspencer@gmail.com>
2017-09-06 13:46:18 -0400

    Finished styling

--- src/components/App.js
+++ src/components/App.js
@@ -53,49 +53,59 @@ class App extends Component {
   render() {
     return (
       <div className="formWrap">
-        <h2>
+        {/* <h2>
           Refund = ${this.state.totalRefund}
-        </h2>
-        <form id="form">
-          <label>Total Amount Due </label>
-          <input onChange={this._dueAmount} placeholder="Total Amount Due" />
-          <label>Paid to Date</label>
-          <input onChange={this._paid} placeholder="Paid to Date" />
-          <label>Admin Fee</label>
-          <input onChange={this._adminFee} placeholder="$250" />
-          <label>Percentage of Time Consumed</label>
-          <input onChange={this._timeConsumed} placeholder="Percentage of Time Consumed" />
-          <button onClick={this.calculateRefund}>Go!</button>
-        </form>
-        <h4>OPTION 1</h4>
-        <div className="option1">
-          <p>
-            <div id="anchor">
-              For your time at Bloc, you owe ${`${this.state.due * this.state.timeConsumed} + $${this.state
-                .adminFee}  `}
-              <a href="http://www.bloc.io/faq">non-refundable fee</a>
-              {` = $${this.state.due * this.state.timeConsumed + this.state.adminFee}`}. Since you’ve paid Bloc ${this.state.paid},
-              you will receive a ${`${this.state.paid -
-                (this.state.due * this.state.timeConsumed + this.state.adminFee)}
-           refund within 5 business days back to the original account that was charged. Please confirm that everything looks good and I will complete your withdrawal.`}
-            </div>
-          </p>
+        </h2> */}
+        <div className="h2">
+          <h2>Withdrawal Calculator</h2>
         </div>
-        <h4>OPTION 2</h4>
-        <p>
-          I have provided your refund details below. Please confirm that everything looks good and I will complete your
-          withdrawal.
-        </p>
-        <p>
-          ${this.state.due} total * {this.state.timeConsumed * 100}% time = ${`${this.state.due * this.state.timeConsumed} due`}
-          <br />
-          <div id="anchor">
-            ${`${this.state.due * this.state.timeConsumed} + $${this.state.adminFee} `}
-            <a href="http://www.bloc.io/faq">non-refundable fee</a> = ${`${this.state.due * this.state.timeConsumed +
-              this.state.adminFee} total due`}
+        <div className="flexRow">
+          <form id="form">
+            <label>Total Amount Due </label>
+            <input onChange={this._dueAmount} placeholder="Total Amount Due" />
+            <label>Paid to Date</label>
+            <input onChange={this._paid} placeholder="Paid to Date" />
+            <label>Admin Fee</label>
+            <input onChange={this._adminFee} placeholder="$250" />
+            <label>Percentage of Time Consumed</label>
+            <input onChange={this._timeConsumed} placeholder="Percentage of Time Consumed" />
+            {/* <button onClick={this.calculateRefund}>Go!</button> */}
+          </form>
+          <div id="results">
+            <h4>OPTION 1</h4>
+            <div className="option1">
+              <p>
+                <div id="anchor">
+                  For your time at Bloc, you owe ${`${this.state.due * this.state.timeConsumed} + $${this.state
+                    .adminFee}  `}
+                  <a href="http://www.bloc.io/faq">non-refundable fee</a>
+                  {` = $${this.state.due * this.state.timeConsumed + this.state.adminFee}`}. Since you’ve paid Bloc ${this.state.paid},
+                  you will receive a ${`${this.state.paid -
+                    (this.state.due * this.state.timeConsumed + this.state.adminFee)}
+           refund within 5 business days back to the original account that was charged. Please confirm that everything looks good and I will complete your withdrawal.`}
+                </div>
+              </p>
+            </div>
+            <div id="option2">
+              <h4>OPTION 2</h4>
+              <p>
+                I have provided your refund details below. Please confirm that everything looks good and I will complete
+                your withdrawal.
+              </p>
+              <p>
+                ${this.state.due} total * {this.state.timeConsumed * 100}% time = ${`${this.state.due * this.state.timeConsumed} due`}
+                <br />
+                <div id="anchor">
+                  ${`${this.state.due * this.state.timeConsumed} + $${this.state.adminFee} `}
+                  <a href="http://www.bloc.io/faq">non-refundable fee</a> = ${`${this.state.due *
+                    this.state.timeConsumed +
+                    this.state.adminFee} total due`}
+                </div>
+                ${this.state.paid} paid - ${this.state.due * this.state.timeConsumed + this.state.adminFee} due = ${`${this.state.paid - this.state.due * this.state.timeConsumed - this.state.adminFee} total refund`}
+              </p>
+            </div>
           </div>
-          ${this.state.paid} paid - ${this.state.due * this.state.timeConsumed + this.state.adminFee} due = ${`${this.state.paid - this.state.due * this.state.timeConsumed - this.state.adminFee} total refund`}
-        </p>
+        </div>
       </div>
     )
   }
